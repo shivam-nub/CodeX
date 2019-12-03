0a8b30f949eb9b9701489fc738c230b427b8e8c0
shivam-nub <58317816+shivam-nub@users.noreply.github.com>
2019-12-03 10:16:51 +0530

    Add files via upload

---
+++ db/login.php
@@ -0,0 +1,31 @@
+<?php
+   
+   $db = mysqli_connect('localhost', 'root', '', 'codex');
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassworda = mysqli_real_escape_string($db,$_POST['password']); 
+      $mypassword= md5($mypassworda);
+      $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		echo $count;
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         header("location: index.php");
+		 echo "a";
+      }else {
+	  echo "ab";
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
